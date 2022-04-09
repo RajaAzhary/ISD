fd2758b955d39f4dd83d72ab4eeb94976b8ad016
RajaAzhary <91717716+RajaAzhary@users.noreply.github.com>
2022-04-09 21:10:18 +0700

    Add files via upload

---
+++ Pra Assessment/linear2.java
@@ -0,0 +1,23 @@
+public class linear2 {
+    public static int linearSearch(int array[], int x) {
+    int n = array.length;
+  
+    for (int i = 0; i < n; i++) {
+      if (array[i] == x)
+      return i;
+    }
+    return -1;
+    }
+  
+    public static void main(String args[]) {
+    int array[] = { 30, 4, 7, 10, 8, 100, 33 };
+    int x = 9;
+  
+    int result = linearSearch(array, x);
+  
+    if (result == -1)
+      System.out.print("X = 9 tidak ditemukan");
+    else
+      System.out.print("X = 9 ditemukan pada index: " + result);
+    }
+  }
